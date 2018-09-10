b7324a82fda4a1d8844f80313b025393f4ab2303
吕晴阳 <lvqingyang@bytedance.com>
2018-09-10 12:01:52 +0800

    [add]多点触摸

--- TouchTracker/TTDrawView.m
+++ TouchTracker/TTDrawView.m
@@ -8,7 +8,7 @@
 
 @interface TTDrawView ()
 @property(nonatomic) NSMutableArray *finishLines;
-@property(nonatomic) TTLine *curLine;
+@property(nonatomic) NSMutableDictionary *curLines;
 @end
 
 @implementation TTDrawView {
@@ -18,8 +18,9 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         _finishLines = [NSMutableArray new];
-        self.backgroundColor= [UIColor whiteColor];
-        self.multipleTouchEnabled=YES;
+        _curLines = [NSMutableDictionary new];
+        self.backgroundColor = [UIColor whiteColor];
+        self.multipleTouchEnabled = YES;
     }
     return self;
 }
@@ -32,9 +33,9 @@
         [self drawLine:line];
     }
 
-    if (self.curLine) {
-        [[UIColor redColor] set];
-        [self drawLine:self.curLine];
+    [[UIColor redColor] set];
+    for (TTLine *line in self.curLines.allValues) {
+        [self drawLine:line];
     }
 
 }
@@ -51,20 +52,36 @@
 #pragma mark Touch events
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
-    TTLine *line = [TTLine new];
-    line.start = line.end = [[touches anyObject] locationInView:self];
-    self.curLine = line;
+    for (UITouch *touch in touches) {
+        TTLine *line = [TTLine new];
+        line.start = line.end = [touch locationInView:self];
+        self.curLines[[NSValue valueWithNonretainedObject:touch]] = line;
+    }
     [self setNeedsDisplay];
 }
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
-    self.curLine.end = [[touches anyObject] locationInView:self];
+    for (UITouch *touch in touches) {
+        TTLine *line = self.curLines[[NSValue valueWithNonretainedObject:touch]];
+        line.end = [touch locationInView:self];
+    }
     [self setNeedsDisplay];
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
-    [self.finishLines addObject:self.curLine];
-    self.curLine=nil;
+    for (UITouch *touch in touches) {
+        NSValue *key=[NSValue valueWithNonretainedObject:touch];
+        [self.finishLines addObject:self.curLines[key]];
+        [self.curLines removeObjectForKey:key];
+    }
+    [self setNeedsDisplay];
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
+    for (UITouch *touch in touches) {
+        NSValue *key=[NSValue valueWithNonretainedObject:touch];
+        [self.curLines removeObjectForKey:key];
+    }
     [self setNeedsDisplay];
 }
 
